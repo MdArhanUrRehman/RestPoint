6035e68d1a941ed397329239d9ef5cc5004f52ee
Mohammed Arhan <mdarhan0309@gmail.com>
2025-08-13 22:02:08 +0530

    server4

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -74,28 +74,6 @@ export const createBooking = async (req, res) => {
       totalPrice,
     });
 
-    // const mailOptions = {
-    //   from: process.env.SENDER_EMAIL,
-    //   to: req.user.email,
-    //   subject: 'Hotel Booking Details',
-    //   html: `
-    //     <h2>Your Booking Details</h2>
-    //     <p>Dear ${req.user.username},</p>
-    //     <p>Thank you for your booking! Here are your details:</p>
-    //     <ul>
-    //       <li><strong>Booking ID:</strong> ${booking.id}</li>
-    //       <li><strong>Hotel Name:</strong> ${roomData.hotel.name}</li>
-    //       <li><strong>Location:</strong> ${roomData.hotel.address}</li>
-    //       <li><strong>Date:</strong> ${booking.checkInDate.toDateString()}</li>
-    //       <li><strong>Booking Amount:</strong>  ${process.env.CURRENCY || '$'} ${booking.totalPrice} /night</li>
-    //     </ul>
-    //     <p>We look forward to welcoming you!</p>
-    //     <p>If you need to make any changes, feel free to contact us.</p>
-    //   `,
-    // };
-
-    // await transporter.sendMail(mailOptions);
-
     res.json({ success: true, message: "Booking created successfully" });
 
   } catch (error) {
