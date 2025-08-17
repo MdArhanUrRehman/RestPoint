a364902ea21df883d8d46018679525d79b8113e4
Mohammed Arhan <mdarhan0309@gmail.com>
2025-08-18 01:46:00 +0530

    commit

--- server/models/Hotel.js
+++ server/models/Hotel.js
@@ -6,7 +6,7 @@ const hotelSchema = new Schema(
     name: { type: String, required: true },
     address: { type: String, required: true },
     contact: { type: String, required: true },
-    owner: { type: String, ref: "User", required: true },
+    owner: { type: String, ref: "User", required: true },  // meant to reference of User _id
     city: { type: String, required: true },
   },
   { timestamps: true }
